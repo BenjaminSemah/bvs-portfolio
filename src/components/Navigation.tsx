041c00292f617030bfee4656e7d33eb13a964c00
Benjamin Semah <benjaminsemah@gmail.com>
2022-08-05 15:44:56 +0000

    Add navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from "react-router-dom";
 import { HashLink as Link } from "react-router-hash-link"
 
 export function Navigation() {
